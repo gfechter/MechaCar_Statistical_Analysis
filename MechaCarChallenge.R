8bff6bbd44c878b6e30053153578039f077858d1
gfechter <103774401+gfechter@users.noreply.github.com>
2022-08-07 22:00:08 -0500

    Adding deliverable 3 code.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,15 @@ total_summary
 
 lot_summary<-Suspension_Coil %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), sdev = sd(PSI))
 lot_summary
+
+# Deliverable 3
+t.test(Suspension_Coil$PSI,mu=1500)
+
+Lot1 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot1")
+Lot2 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot2")
+Lot3 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot3")
+
+
+t.test(Lot1$PSI,mu=1500)
+t.test(Lot2$PSI,mu=1500)
+t.test(Lot3$PSI,mu=1500)
