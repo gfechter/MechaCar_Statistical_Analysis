a5340d46e05c3f8b1448a94865d2319a61529971
gfechter <103774401+gfechter@users.noreply.github.com>
2022-08-07 19:22:57 -0500

    Adding Deliverable 2 Code.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,12 @@ library(dplyr)
 view (MechaCar_mpg)
 model<-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
 summary(model)
+
+
+# Deliverable 2
+view(Suspension_Coil)
+total_summary<-Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), sdev = sd(PSI))
+total_summary
+
+lot_summary<-Suspension_Coil %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), sdev = sd(PSI))
+lot_summary
